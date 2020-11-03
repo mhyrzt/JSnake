23431798b6ffa48ffd4bb409154bb70cc317fca3
Mahyar <mr.riazati1999@gmail.com>
2020-11-03 20:54:38 +0330

    adding key reading from keyboard

--- script.js
+++ script.js
@@ -6,18 +6,36 @@ function Snake(){
     this.addTail   = function () {
         tails.push(tails.length);
     }
-    this.updateDir = function (dir) {
-        direction = dir;
+    
+    this.getOpos = function () {
+        return this.opposits[this.direction]
     }
+    
+    this.ligalMove = function (dir) {
+        return !(this.direction == dir || dir == this.getOpos());
+    }
+
+    this.updateDir = function (dir) {
+        if (this.ligalMove(dir))
+            this.direction = dir;
+    }
+    
     this.head  = document.getElementById("head");
     this.tails = [];
-    this.directions = {
-        1: "up",
-        2: "down",
-        3: "left",
-        4: "right"
+    this.directions = [
+        'UP'    ,
+        'DOWN'  ,
+        'LEFT'  ,
+        'RIGHT'
+    ];
+    this.opposits = {
+        'UP'   :'DOWN'  ,
+        'DOWN' :'UP'    ,
+        'RIGHT':'LEFT'  ,
+        'LEFT' :'RIGHT'
     };
-    this.direction = generateRandom(4);
+    this.direction = this.directions[generateRandom(3)];
+
 }
 
 function playGround(){
@@ -25,16 +43,19 @@ function playGround(){
         document.getElementById("score").innerHTML  = `Score:  ${this.playeScore}`;
         document.getElementById("record").innerHTML = `Record: ${this.playerRecord}`; 
     }
+
     this.getRecord    = function () {
         let record = localStorage.getItem('record');
         if (record) return record;
         localStorage.setItem('record', 0);
         return 0;
     }
+
     this.updateRecord = function () {
         if (playeScore > playerRecord)
             playerRecord = playeScore;
     }
+
     this.generateFood = function () {
         let x  = generateRandom(100);
         let y  = generateRandom(100);        
@@ -50,6 +71,22 @@ function playGround(){
 }
 
 var pg;
+let KEYS = {
+    37: 'LEFT',
+    38: 'UP',
+    39: 'RIGHT',
+    40: 'DOWN',
+    65: 'LEFT',
+    68: 'RIGHT',
+    83: 'DOWN',
+    87: 'UP'
+};
+
 $(document).ready(function() {
     pg = new playGround();
-})
+    document.body.addEventListener("keydown", function(event) {
+        let key = event.keyCode;
+        let dir = KEYS[key];
+        pg.snake.updateDir(dir);
+    });
+});
